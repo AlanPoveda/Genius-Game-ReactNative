686923950476ea9c8e675b2b9e005d37073d7ae0
AlanPoveda <alsepoveda@gmail.com>
2021-02-17 12:21:39 -0300

    Feita a tela de Game Over

---
+++ src/components/GameOver/index.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import { Text, View, Button, TouchableOpacity } from 'react-native';
+import styled from 'styled-components';
+
+import { geniusArray } from '../../../logic'
+
+
+
+
+const GameOverView = styled.View`
+    flex: 1;
+    align-items: center;
+    justify-content: center;
+
+`;
+
+const GameOverText = styled.Text`
+    color: white;
+    font-size: 50px;
+`;
+
+GameOverText.Subtext = styled.Text`
+    color: white;
+    font-size: 30px;
+    padding-bottom: 50px;
+`;
+
+
+GameOverView.ButtonRestart = styled.Text`
+    
+    padding: 50px 50px;
+    background-color: #A83279;
+    font-size: 40px;
+    border-radius: 20px;
+    color: white;
+`;
+
+
+
+
+
+export default function GameOver(){
+
+
+    return(
+        
+        <GameOverView>
+            <GameOverText>
+                Game Over               
+            </GameOverText>
+            <GameOverText.Subtext>
+                Score: {geniusArray.length}
+            </GameOverText.Subtext>
+            <TouchableOpacity>
+            <GameOverView.ButtonRestart>
+                Try Again
+            </GameOverView.ButtonRestart>
+            </TouchableOpacity>
+
+        </GameOverView>
+    )    
+}
