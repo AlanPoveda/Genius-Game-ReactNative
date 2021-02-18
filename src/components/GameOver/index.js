c641c4dba4d8b3715901c0977cbb864da1a4affb
AlanPoveda <alsepoveda@gmail.com>
2021-02-18 12:28:06 -0300

    Usando o pretier no código e ainda já arrumado o bug do score.

--- src/components/GameOver/index.js
+++ src/components/GameOver/index.js
@@ -1,65 +1,47 @@
-import React from 'react';
-import { Text, View, Button, TouchableOpacity } from 'react-native';
-import styled from 'styled-components';
+import React from "react";
+import { Text, View, Button, TouchableOpacity } from "react-native";
+import styled from "styled-components";
 
-import { geniusArray } from '../../../logic'
-
-
-
+import { geniusArray } from "../../../GameLogic";
 
 const GameOverView = styled.View`
-    flex: 1;
-    align-items: center;
-    justify-content: center;
-
+  flex: 1;
+  align-items: center;
+  justify-content: center;
 `;
 
 const GameOverText = styled.Text`
-    color: white;
-    font-size: 50px;
+  color: white;
+  font-size: 50px;
 `;
 
 GameOverText.Subtext = styled.Text`
-    color: white;
-    font-size: 30px;
-    padding-bottom: 50px;
+  color: white;
+  font-size: 30px;
+  padding-bottom: 50px;
 `;
-
 
 GameOverView.ButtonRestart = styled.Text`
-    
-    padding: 50px 50px;
-    background-color: #A83279;
-    font-size: 40px;
-    border-radius: 20px;
-    color: white;
+  padding: 50px 50px;
+  background-color: #a83279;
+  font-size: 40px;
+  border-radius: 20px;
+  color: white;
 `;
 
-
-
-
-
-export default function GameOver(props){
-
-
-    return(
-        
-        <GameOverView>
-            <GameOverText>
-                Game Over               
-            </GameOverText>
-            <GameOverText.Subtext>
-                Score: {props.arrayGame.length - 1}
-            </GameOverText.Subtext>
-            <TouchableOpacity>
-                <GameOverView.ButtonRestart>
-                    Try Again
-                </GameOverView.ButtonRestart>
-            </TouchableOpacity>
-            <GameOverText.Subtext style={{paddingTop: 20}}>
-                Good Luck Next!
-            </GameOverText.Subtext>
-
-        </GameOverView>
-    )    
+export default function GameOver(props) {
+  return (
+    <GameOverView>
+      <GameOverText>Game Over</GameOverText>
+      <GameOverText.Subtext>
+        Score: {props.arrayGame}
+      </GameOverText.Subtext>
+      <TouchableOpacity>
+        <GameOverView.ButtonRestart>Try Again</GameOverView.ButtonRestart>
+      </TouchableOpacity>
+      <GameOverText.Subtext style={{ paddingTop: 20 }}>
+        Good Luck Next!
+      </GameOverText.Subtext>
+    </GameOverView>
+  );
 }
