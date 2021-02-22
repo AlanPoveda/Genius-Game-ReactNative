2b226da5211b2bfcecb7ff40017fd1f4bb821361
AlanPoveda <alsepoveda@gmail.com>
2021-02-21 21:28:23 -0300

    :sparkles: Começando a parte da animação e verificação da array, ainda entendendo como funciona o state para fazer a animação

--- src/components/Game/index.js
+++ src/components/Game/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import {
   Text,
   View,
@@ -6,6 +6,7 @@ import {
   TouchableOpacity,
   ProgressViewIOSComponent,
   Animated,
+  StyleSheet
 } from "react-native";   
 import styled from "styled-components";
 
@@ -43,7 +44,8 @@ Buttons.Bot = styled.Text`
   flex-direction: row;
 `;
 
-Buttons.Blue = styled.Text`
+
+Buttons.Blue = styled.View`
   width: 150px;
   height: 150px;
   background-color: #1e90ff;
@@ -52,7 +54,7 @@ Buttons.Blue = styled.Text`
 
 `;
 
-Buttons.Green = styled.Text`
+Buttons.Green = styled.View`
   width: 150px;
   height: 150px;
   background-color: #3cb371;
@@ -60,7 +62,7 @@ Buttons.Green = styled.Text`
   overflow: hidden;
 `;
 
-Buttons.Red = styled.Text`
+Buttons.Red = styled.View`
   width: 150px;
   height: 150px;
   background-color: #fa8072;
@@ -68,7 +70,7 @@ Buttons.Red = styled.Text`
   overflow: hidden;
 `;
 
-Buttons.Yellow = styled.Text`
+Buttons.Yellow = styled.View`
   width: 150px;
   height: 150px;
   background-color: #ffd700;
@@ -76,7 +78,51 @@ Buttons.Yellow = styled.Text`
   overflow: hidden;
 `;
 
+
+
+
+
 export default function Game(props) {
+  const opacity = useState(new Animated.Value(0))[0]
+  const [ opacityState, setOpacityState] = useState()
+
+  animationArray()
+
+  function opacityAnimationActive(){
+    Animated.timing(opacity,{
+      toValue: 1,
+      duration: 1000,
+      useNativeDriver: true,
+    }).start()
+    
+    
+  }
+  
+  
+  
+  let opacity00 = {opacity} //1
+  let opacity01 = {opacity} //1
+  let opacity02 = {opacity} //1
+  let opacity03 = {opacity} //1
+  console.log(`Esta é a quantidade de itens ${props.arrayGameShow.length}`)
+  function animationArray(){
+    for (let index= 0 ; index < props.arrayGameShow.length ; index++){
+      if(props.arrayGame[index] === 0)  {
+        opacity00 = {opacity} //0
+        console.log(`Estou aqui 0`)
+      }else if(props.arrayGame[index] === 1)  {
+        console.log(`Estou aqui 1`)
+      }else if(props.arrayGame[index] === 2)  {
+        console.log(`Estou aqui 2`)
+      }else if(props.arrayGame[index] === 3)  {
+        console.log(`Estou aqui 3`)
+      }
+
+    }
+    opacityAnimationActive()
+  }
+
+
 
   return (
     <Buttons >
@@ -92,7 +138,10 @@ export default function Game(props) {
             props.setArrayGameShow();
           }}
         >
-          <Buttons.Blue >0</Buttons.Blue>
+          <Animated.View style={opacity00}>
+            <Buttons.Blue ><Text style={{padding: 15}}>0</Text></Buttons.Blue>
+          </Animated.View>
+          
         </TouchableOpacity>
 
         <TouchableOpacity
@@ -103,7 +152,9 @@ export default function Game(props) {
             props.setArrayGameShow();
           }}
         >
-          <Buttons.Green >1</Buttons.Green>
+          <Animated.View style={opacity01}>
+            <Buttons.Green ><Text>1</Text></Buttons.Green>
+            </Animated.View>
         </TouchableOpacity>
       </Buttons.Top>
 
@@ -116,7 +167,9 @@ export default function Game(props) {
             props.setArrayGameShow();
           }}
         >
-          <Buttons.Red >2</Buttons.Red>
+          <Animated.View style={opacity02}>
+            <Buttons.Red ><Text>2</Text></Buttons.Red>
+            </Animated.View>
         </TouchableOpacity>
 
         <TouchableOpacity
@@ -127,9 +180,13 @@ export default function Game(props) {
             props.setArrayGameShow();
           }}
         >
-          <Buttons.Yellow >3</Buttons.Yellow>
+          <Animated.View style={opacity03}>
+            <Buttons.Yellow ><Text>3</Text></Buttons.Yellow>
+            </Animated.View>
         </TouchableOpacity>
       </Buttons.Bot>
     </Buttons>
   );
 }
+
+
